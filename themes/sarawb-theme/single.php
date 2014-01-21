21f78ef09feb6c555a41676926bbced12b8f2d08
Tim Smith <smith@ttimsmith.com>
2014-01-21 12:51:16 -0600

    Displaying recent post and comments

--- themes/sarawb-theme/single.php
+++ themes/sarawb-theme/single.php
@@ -10,7 +10,41 @@
         </header>
 
         <?php the_content(); ?>
+
+        <footer class="post__footer">
+          <hr>
+          <h5>Recently</h5>
+          <div class="post__recent-post">
+            <?php
+              $this_post = $post->ID;
+              $query = new WP_Query( array(
+                  'post__not_in'  => array( $this_post ),
+                  'showposts' => 1
+                ) ); ?>
+
+              <?php if ( $query->have_posts() ) : ?>
+                <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                  <h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+                  <?php the_excerpt(); ?>
+              <?php endwhile; ?>
+
+            <?php wp_reset_postdata(); endif; ?>
+          </div><!-- .post__recent-post -->
+
+          <div class="post__newsletter">
+            <h5>Get my blog posts, straight to your inbox</h5>
+            <!-- FIXME: Input Goes Here --> Input Goes here
+          </div>
+          <hr>
+        </footer>
       </article>
+
+      <section class="comments">
+        <h5>Comments</h5>
+        <?php wp_list_comments(); ?>
+
+        <?php comment_form(); ?>
+      </section>
     </div><!-- grid8 centered -->
 
 <?php get_footer(); ?>
