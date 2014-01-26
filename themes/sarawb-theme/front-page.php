1073ca919006153988a5b9c71926bae1057edec3
Tim Smith <smith@ttimsmith.com>
2014-01-25 22:38:48 -0600

    Adding initial elements to homepage

---
+++ themes/sarawb-theme/front-page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+  <section class="content__section site__headline">
+    <h1>No buzzwords, no bandwagons: I’m a content strategy consultant who’ll help you make real change.</h1>
+  </section>
+
+  <section class="content__section">
+    <div class="grid6 columns">
+      <p>You don’t need another document shoved in a desk drawer or set of rules tacked to a wall. You need your organization to live out its vision. Teams to collaborate. Authors to feel like they’re part of the big picture.</p>
+
+      <p>I’ll help you get there by pulling together the people who affect your content, aligning them around real goals, and empowering them to bring strategy to life. Then we’ll turn your toughest content problems—from messaging to content modeling to mobile—into realistic initiatives people can actually get excited about.</p>
+
+      <p>We can change your content—and the culture that produces it. Together.</p>
+    </div>
+    <div class="grid6 columns">
+      <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
+    </div>
+  </section>
+
+<?php get_footer(); ?>
