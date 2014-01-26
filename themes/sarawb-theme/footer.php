5a2a52536833a6ce48ce3bf226d43f43a548d31c
Tim Smith <smith@ttimsmith.com>
2014-01-25 22:36:41 -0600

    Placing initial footer elements

--- themes/sarawb-theme/footer.php
+++ themes/sarawb-theme/footer.php
@@ -1,6 +1,18 @@
-
   </div><!-- contain -->
 </main>
+
+<footer class="site__footer" role="contentinfo">
+  <div class="contain">
+    <div class="grid4 columns">
+      <h4>Let’s Talk</h4>
+      <p>Tell me about your project, your conference, or just what's keeping you up at night.</p>
+    </div>
+    <div class="grid4 columns">
+      <p>Brought to you from beautiful South Philadelphia.</p>
+      <p>541.517.2870</p>
+    </div>
+  </div>
+</footer>
 
 <!--WP Generated Footer -->
 <?php wp_footer(); ?>
