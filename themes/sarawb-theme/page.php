397153c61e76f534a8c23a9e43d03ab7029f14ed
Tim Smith <smith@ttimsmith.com>
2014-01-25 22:36:20 -0600

    Designing Page template

--- themes/sarawb-theme/page.php
+++ themes/sarawb-theme/page.php
@@ -1,4 +1,11 @@
 <?php get_header(); ?>
 
+<?php the_post(); ?>
+
+<div class="grid7 centered">
+  <h1 class="page__title"><?php the_title(); ?></h1>
+  <?php the_content(); ?>
+</div>
+
 
 <?php get_footer(); ?>
