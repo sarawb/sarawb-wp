cdc867576d2cd828b038bdf815a7c05e027736c9
Tim Smith <smith@ttimsmith.com>
2014-01-25 22:37:46 -0600

    Linking logo, and linking to about page

--- themes/sarawb-theme/header.php
+++ themes/sarawb-theme/header.php
@@ -45,7 +45,7 @@
 <header class="site__header" role="banner">
   <div class="contain">
     <div class="site__branding left">
-      <img src="<?php bloginfo('template_url'); ?>/assets/img/SWB_Logo.svg" alt="Sara Wachter-Boettcher" width="90">
+      <a href="/"><img src="<?php bloginfo('template_url'); ?>/assets/img/SWB_Logo.svg" alt="Sara Wachter-Boettcher" width="90"></a>
     </div>
 
     <div class="nav_and_search grid5 right">
@@ -54,7 +54,7 @@
       </div>
       <nav class="site__nav right" role="navigation">
         <ul>
-          <li><a href="#">About</a></li>
+          <li><a href="/about">About</a></li>
           <li><a href="#">Blog</a></li>
         </ul>
       </nav>
