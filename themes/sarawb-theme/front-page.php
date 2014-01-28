49dd71c506f6c3b8ef011734ea08af973bb1c7b6
Tim Smith <smith@ttimsmith.com>
2014-01-28 17:18:50 -0600

    Adding workshops, consulting and project partnerships copy to homepage [#6]

--- themes/sarawb-theme/front-page.php
+++ themes/sarawb-theme/front-page.php
@@ -11,10 +11,34 @@
       <p>I’ll help you get there by pulling together the people who affect your content, aligning them around real goals, and empowering them to bring strategy to life. Then we’ll turn your toughest content problems—from messaging to content modeling to mobile—into realistic initiatives people can actually get excited about.</p>
 
       <p>We can change your content—and the culture that produces it. Together.</p>
+
+      <h3>Workshops</h3>
+      <p>I mix educational sessions with hands-on activities that get teams thinking and working differently. Half-day, full-day, or longer sessions available.</p>
+
+      <h3>Consulting</h3>
+      <p>I’ll work with you to facilitate the strategic process, define and plan projects, and identify the right people to augment your team.</p>
+
+      <h3>Project partnerships</h3>
+      <p>I partner with agencies or internal teams to develop messaging and editorial strategy for websites, apps, and digital products.</p>
     </div>
     <div class="grid6 columns">
       <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
     </div>
   </section>
+
+  <!-- <div class="content__section">
+    <div class="grid4 columns">
+      <h3>Workshops</h3>
+      <p>I mix educational sessions with hands-on activities that get teams thinking and working differently. Half-day, full-day, or longer sessions available.</p>
+    </div>
+    <div class="grid4 columns">
+      <h3>Consulting</h3>
+      <p>I’ll work with you to facilitate the strategic process, define and plan projects, and identify the right people to augment your team.</p>
+    </div>
+    <div class="grid4 columns">
+      <h3>Project partnerships</h3>
+      <p>I partner with agencies or internal teams to develop messaging and editorial strategy for websites, apps, and digital products.</p>
+    </div>
+  </div> -->
 
 <?php get_footer(); ?>
