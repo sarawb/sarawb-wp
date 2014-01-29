31a3d258fd170b99049b65ecba7d48da6bc01f48
Tim Smith <smith@ttimsmith.com>
2014-01-28 22:26:03 -0600

    Putting in icon font [#5]

--- themes/sarawb-theme/footer.php
+++ themes/sarawb-theme/footer.php
@@ -10,6 +10,24 @@
     <div class="grid4 columns">
       <p>Brought to you from beautiful South Philadelphia.</p>
       <p>541.517.2870</p>
+      <ul class="inline-list social-icons">
+        <li><a class="icon__twitter" href="#">
+          <span class="screen-reader-hide">Twitter Icon</span>
+          <span class="icon" data-icon="t"></span>
+        </a></li>
+        <li><a class="icon__linkedin" href="">
+          <span class="screen-reader-hide">LinkedIn Icon</span>
+          <span class="icon" data-icon="i"></span>
+        </a></li>
+        <li><a class="icon__ss" href="">
+          <span class="screen-reader-hide">Slideshare Icon</span>
+          <span class="icon" data-icon="s"></span>
+        </a></li>
+        <li><a class="icon__lanyrd" href="">
+          <span class="screen-reader-hide">Lanyrd Icon</span>
+          <span class="icon" data-icon="l"></span>
+        </a></li>
+      </ul>
     </div>
   </div>
 </footer>
