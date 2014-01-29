44b75291fa747b004627db9085794be3c932a7cf
Tim Smith <smith@ttimsmith.com>
2014-01-28 23:35:57 -0600

    Getting pagenavi out of loop.

--- themes/sarawb-theme/index.php
+++ themes/sarawb-theme/index.php
@@ -2,23 +2,24 @@
 
 
     <div class="grid7 centered">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-      <article class="entry">
-        <header class="post__meta">
-          <h1 class="post__title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-          <small class="pubdate"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time> by <?php the_author(); ?></small>
-        </header>
+          <article class="entry">
+            <header class="post__meta">
+              <h1 class="post__title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+              <small class="pubdate"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time> by <?php the_author(); ?></small>
+            </header>
 
-        <?php the_excerpt(); ?>
-        <hr>
-      </article>
+            <?php the_excerpt(); ?>
+            <hr>
+          </article>
 
-      <div class="page__navigation">
-        <?php wp_pagenavi(); ?>
-      </div>
+    	<?php endwhile; endif; ?>
 
-	<?php endwhile; endif; ?>
+    <div class="page__navigation">
+      <?php wp_pagenavi(); ?>
+    </div>
+
     </div>
 
 <?php get_footer(); ?>
