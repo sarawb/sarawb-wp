11c0fc4354d1c0fbd9ff17d496ae29ecd123c16f
Tim Smith <smith@ttimsmith.com>
2014-01-28 21:47:10 -0600

    Almost finished with homepage elements [#6]

--- themes/sarawb-theme/front-page.php
+++ themes/sarawb-theme/front-page.php
@@ -5,24 +5,42 @@
   </section>
 
   <section class="content__section">
-    <div class="grid6 columns">
+    <div class="grid5 columns">
       <p>You don’t need another document shoved in a desk drawer or set of rules tacked to a wall. You need your organization to live out its vision. Teams to collaborate. Authors to feel like they’re part of the big picture.</p>
 
       <p>I’ll help you get there by pulling together the people who affect your content, aligning them around real goals, and empowering them to bring strategy to life. Then we’ll turn your toughest content problems—from messaging to content modeling to mobile—into realistic initiatives people can actually get excited about.</p>
 
       <p>We can change your content—and the culture that produces it. Together.</p>
 
-      <h3>Workshops</h3>
+      <h4>Workshops</h4>
       <p>I mix educational sessions with hands-on activities that get teams thinking and working differently. Half-day, full-day, or longer sessions available.</p>
 
-      <h3>Consulting</h3>
+      <h4>Consulting</h4>
       <p>I’ll work with you to facilitate the strategic process, define and plan projects, and identify the right people to augment your team.</p>
 
-      <h3>Project partnerships</h3>
+      <h4>Project partnerships</h4>
       <p>I partner with agencies or internal teams to develop messaging and editorial strategy for websites, apps, and digital products.</p>
     </div>
-    <div class="grid6 columns">
-      <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
+    <div class="grid7 columns">
+      <figure class="fullsize">
+        <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
+      </figure>
+      <section class="content__section">
+        <figure class="alignleft">
+          <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_cover.png" alt="Content Everywhere">
+        </figure>
+        <h2 class="no-margin">Content Everywhere</h2>
+        <p>From smartphones to read-later services to whatever’s next: your content needs to go more places, more easily. I wrote a book to show you how. <a href="URL">Buy it now</a>.</p>
+      </section>
+      <section class="content__section">
+        <figure class="alignleft">
+          <img src="<?php bloginfo('template_url'); ?>/assets/img/megaphone.svg" alt="Megaphone">
+        </figure>
+        <h2 class="no-margin">Speaking</h2>
+        <p>I’ve shared my ideas at conferences like Confab, Web Directions, Mobilism, Content Strategy Forum, Breaking Development, and UX Week. See <a href="URL">where I’m speaking next</a>.</p>
+      </section>
+      <h2 class="no-margin">A List Apart</h2>
+      <p>The web wouldn’t be the same without <cite>A List Apart</cite>, a magazine about content, culture, and code since 1998. I’ve been the editor in chief since 2012. <a href="URL">Write for us</a>.</p>
     </div>
   </section>
 
