8b44a0b62a458533c0c6d0ed9992717e34f4f080
Tim Smith <smith@ttimsmith.com>
2014-01-31 16:26:57 -0600

    Changing social icon links [#5]
    I also changed the logic for the footer to conditionally display based on template rather than have two separate footers.

--- themes/sarawb-theme/footer.php
+++ themes/sarawb-theme/footer.php
@@ -3,6 +3,12 @@
 
 <footer class="site__footer" role="contentinfo">
   <div class="contain">
+  <?php if(is_single()) : ?>
+    <div class="grid4 columns">
+      <h4>About Me</h4>
+      <p>I’m a content strategy consultant, writer, and editor currently living in lovely Philadelphia. <a href="<?php bloginfo('url'); ?>/about">Read more&hellip;</a></p>
+    </div>
+  <?php else : ?>
     <div class="grid4 columns">
       <h4>Let’s talk</h4>
       <p>Tell me about your project, your conference, or just what's keeping you up at night.</p>
@@ -10,23 +16,24 @@
     <div class="grid4 columns">
       <?php gravity_form('Contact Form', false, false, false, '', true); ?>
     </div>
+  <?php endif; ?>
     <div class="grid4 columns">
       <p>Brought to you from beautiful South Philadelphia.</p>
       <p>541.517.2870</p>
       <ul class="inline-list social-icons">
-        <li><a class="icon__twitter" href="#">
+        <li><a class="icon__twitter" href="https://twitter.com/sara_ann_marie">
           <span class="screen-reader-hide">Twitter Icon</span>
           <span class="icon" data-icon="t"></span>
         </a></li>
-        <li><a class="icon__linkedin" href="">
+        <li><a class="icon__linkedin" href="http://linkedin.com/in/saraboettcher">
           <span class="screen-reader-hide">LinkedIn Icon</span>
           <span class="icon" data-icon="i"></span>
         </a></li>
-        <li><a class="icon__ss" href="">
+        <li><a class="icon__ss" href="http://slideshare.net/saraboettcher">
           <span class="screen-reader-hide">Slideshare Icon</span>
           <span class="icon" data-icon="s"></span>
         </a></li>
-        <li><a class="icon__lanyrd" href="">
+        <li><a class="icon__lanyrd" href="http://lanyrd.com/profile/sara_ann_marie/">
           <span class="screen-reader-hide">Lanyrd Icon</span>
           <span class="icon" data-icon="l"></span>
         </a></li>
--- themes/sarawb-theme/single.php
+++ themes/sarawb-theme/single.php
@@ -45,4 +45,4 @@
       </section>
     </div><!-- grid8 centered -->
 
-<?php get_footer('blog'); ?>
+<?php get_footer(); ?>
