16f46fe7ece67db0ee027af6aaff32168e4f9798
Tim Smith <smith@ttimsmith.com>
2014-01-31 16:06:36 -0600

    Changing homepage layout [#6]

--- themes/sarawb-theme/front-page.php
+++ themes/sarawb-theme/front-page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-  <section class="content__section site__headline">
-    <h1>No buzzwords, no bandwagons: I’m a content strategy consultant who’ll help you make real change.</h1>
-  </section>
-
   <section class="content__section">
-    <div class="grid5 columns">
+    <div class="grid7 centered">
+      <header class="site__headline">
+        <h1>No buzzwords, no bandwagons: I’m a content strategy consultant who’ll help you make real change.</h1>
+      </header>
+      <figure class="alignright">
+        <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
+      </figure>
       <p>You don’t need another document shoved in a desk drawer or set of rules tacked to a wall. You need your organization to live out its vision. Teams to collaborate. Authors to feel like they’re part of the big picture.</p>
 
       <p>I’ll help you get there by pulling together the people who affect your content, aligning them around real goals, and empowering them to bring strategy to life. Then we’ll turn your toughest content problems—from messaging to content modeling to mobile—into realistic initiatives people can actually get excited about.</p>
@@ -21,26 +23,53 @@
       <h4>Project partnerships</h4>
       <p>I partner with agencies or internal teams to develop messaging and editorial strategy for websites, apps, and digital products.</p>
     </div>
-    <div class="grid7 columns">
-      <figure class="fullsize">
-        <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
-      </figure>
-      <section class="content__section">
-        <figure class="alignleft">
-          <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_cover.png" alt="Content Everywhere">
+  </section>
+
+  <section class="content__section secondary__projects">
+    <div class="grid6 columns">
+      <div class="grid2 columns">
+        <figure class="fullsize">
+          <img src="<?php bloginfo('template_url'); ?>/assets/img/mic_icon.svg" alt="Megaphone">
         </figure>
-        <h2 class="no-margin">Content Everywhere</h2>
-        <p>From smartphones to read-later services to whatever’s next: your content needs to go more places, more easily. I wrote a book to show you how. <a href="URL">Buy it now</a>.</p>
-      </section>
-      <section class="content__section">
-        <figure class="alignleft">
-          <img src="<?php bloginfo('template_url'); ?>/assets/img/megaphone.svg" alt="Megaphone">
-        </figure>
-        <h2 class="no-margin">Speaking</h2>
+      </div>
+
+      <div class="grid10 columns">
+        <h4 class="no-margin">Speaking</h4>
         <p>I’ve shared my ideas at conferences like Confab, Web Directions, Mobilism, Content Strategy Forum, Breaking Development, and UX Week. See <a href="URL">where I’m speaking next</a>.</p>
+      </div>
+    </div>
+
+    <div class="grid6 columns">
+      <div class="grid2 columns">
+        <figure class="fullsize">
+          <img src="<?php bloginfo('template_url'); ?>/assets/img/edit_icon.svg" alt="Megaphone">
+        </figure>
+      </div>
+
+      <div class="grid10 columns">
+        <h4 class="no-margin">A List Apart</h4>
+        <p>The web wouldn’t be the same without <cite>A List Apart</cite>, a magazine about content, culture, and code since 1998. I’ve been the editor in chief since 2012. <a href="URL">Write for us</a>.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="content__section">
+    <div class="grid8 centered panel">
+
+      <section class="content__section">
+        <div class="grid3 columns">
+          <figure class="fullsize">
+            <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_cover.png" alt="Content Everywhere">
+          </figure>
+        </div>
+
+        <div class="grid8 columns">
+          <h3 class="no-margin">Content Everywhere</h3>
+          <p>From smartphones to read-later services to whatever’s next: your content needs to go more places, more easily. I wrote a book to show you how.</p>
+          <a class="button" href="URL">Buy it now</a>
+        </div>
+
       </section>
-      <h2 class="no-margin">A List Apart</h2>
-      <p>The web wouldn’t be the same without <cite>A List Apart</cite>, a magazine about content, culture, and code since 1998. I’ve been the editor in chief since 2012. <a href="URL">Write for us</a>.</p>
     </div>
   </section>
 
