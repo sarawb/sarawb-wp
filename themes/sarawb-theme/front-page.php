d41e853747c32884087d6572ebf71ac4baf0fe2b
Tim Smith <smith@ttimsmith.com>
2014-02-11 16:43:46 -0600

    Updating homepage layout with 3 column grid for services

--- themes/sarawb-theme/front-page.php
+++ themes/sarawb-theme/front-page.php
@@ -4,26 +4,32 @@
     <header class="site__headline">
       <h1>No buzzwords, no bandwagons: I’m a content strategy consultant who’ll help you make real change.</h1>
     </header>
-    <div class="grid7 columns">
+    <div class="grid6 columns">
       <p>You don’t need another document shoved in a desk drawer or set of rules tacked to a wall. You need your organization to live out its vision. Teams to collaborate. Authors to feel like they’re part of the big picture.</p>
 
       <p>I’ll help you get there by pulling together the people who affect your content, aligning them around real goals, and empowering them to bring strategy to life. Then we’ll turn your toughest content problems—from messaging to content modeling to mobile—into realistic initiatives people can actually get excited about.</p>
 
       <p>We can change your content—and the culture that produces it. Together.</p>
-
-      <h3>Workshops</h3>
-      <p>I mix educational sessions with hands-on activities that get teams thinking and working differently. Half-day, full-day, or longer sessions available.</p>
-
-      <h3>Consulting</h3>
-      <p>I’ll work with you to facilitate the strategic process, define and plan projects, and identify the right people to augment your team.</p>
-
-      <h3>Project partnerships</h3>
-      <p>I partner with agencies or internal teams to develop messaging and editorial strategy for websites, apps, and digital products.</p>
     </div>
-    <div class="grid5 columns">
+    <div class="grid6 columns">
       <figure class="fullsize">
         <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
       </figure>
+    </div>
+  </section>
+
+  <section class="content__section services">
+    <div class="grid4 columns">
+      <h3>Workshops</h3>
+      <p>I mix educational sessions with hands-on activities that get teams thinking and working differently. Half-day, full-day, or longer sessions available.</p>
+    </div>
+    <div class="grid4 columns">
+      <h3>Consulting</h3>
+      <p>I’ll work with you to facilitate the strategic process, define and plan projects, and identify the right people to augment your team.</p>
+    </div>
+    <div class="grid4 columns">
+      <h3>Project partnerships</h3>
+      <p>I partner with agencies or internal teams to develop messaging and editorial strategy for websites, apps, and digital products.</p>
     </div>
   </section>
 
