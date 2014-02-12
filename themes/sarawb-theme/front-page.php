68fced3936995de4e7e146fc0029f56dadd03d26
Tim Smith <smith@ttimsmith.com>
2014-02-12 13:44:37 -0600

    Make homepage photo small [#13]

--- themes/sarawb-theme/front-page.php
+++ themes/sarawb-theme/front-page.php
@@ -4,14 +4,14 @@
     <header class="site__headline">
       <h1>No buzzwords, no bandwagons: I’m a content strategy consultant who’ll help you make real change.</h1>
     </header>
-    <div class="grid6 columns">
+    <div class="grid7 columns">
       <p>You don’t need another document shoved in a desk drawer or set of rules tacked to a wall. You need your organization to live out its vision. Teams to collaborate. Authors to feel like they’re part of the big picture.</p>
 
       <p>I’ll help you get there by pulling together the people who affect your content, aligning them around real goals, and empowering them to bring strategy to life. Then we’ll turn your toughest content problems—from messaging to content modeling to mobile—into realistic initiatives people can actually get excited about.</p>
 
       <p>We can change your content—and the culture that produces it. Together.</p>
     </div>
-    <div class="grid6 columns">
+    <div class="grid5 columns">
       <figure class="fullsize">
         <img src="<?php bloginfo('template_url'); ?>/assets/img/swb_photo.jpg" alt="Sara Wachter-Boettcher">
       </figure>
