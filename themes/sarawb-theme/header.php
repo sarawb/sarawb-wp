528810accb74b40a60446932c8b95faf3434a308
Tim Smith <smith@ttimsmith.com>
2014-02-18 14:54:35 -0600

    Changing logic around the title for each document

--- themes/sarawb-theme/header.php
+++ themes/sarawb-theme/header.php
@@ -23,7 +23,7 @@
   -->
 
   <meta charset="utf-8" />
-  <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
+  <title><?php bloginfo('name'); ?>  | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 
