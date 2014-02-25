92122d66ef02bdce6839f06e4ee6c306f25f44d2
Tim Smith <smith@ttimsmith.com>
2014-02-24 18:56:05 -0600

    Small adjustment to container and writing some copy for 404

--- themes/sarawb-theme/404.php
+++ themes/sarawb-theme/404.php
@@ -1,5 +1,7 @@
 <?php @header("HTTP/1.1 404 Not found", TRUE, 404); ?>
 <?php get_header() ?>
-
-
+  <div class="grid7 centered">
+    <h1 class="page__title">404: Page Not Found</h1>
+    <p>I'm terribly sorry, the page you’re looking for has been deleted or moved. Try searching for it in the box above.</p>
+  </div>
 <?php get_footer(); ?>
